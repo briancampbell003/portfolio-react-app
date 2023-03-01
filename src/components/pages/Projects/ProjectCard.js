7788ac83e116d9117a532158ce75ecca02fccb35
Brian Campbell <brian.campbell003@gmail.com>
2023-03-01 15:31:32 -0600

    styling improvements

--- src/components/pages/Projects/ProjectCard.js
+++ src/components/pages/Projects/ProjectCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import gitLogo from './assets/git-logo.png';
+import liveLogo from './assets/live-logo.png';
 
 export default function ProjectCard({ projData }) {
   return (
@@ -7,24 +8,35 @@ export default function ProjectCard({ projData }) {
       {projData.map((proj) => (
         <div className='proj-card' key={proj.key} style={{
           backgroundImage: `url("${proj.imgPath}")`
+        }}>
+          <p className='proj-title'>
+            {proj.title}
+          </p>
+          <div className='hide' style={{
+            backgroundImage: `linear-gradient(to right, rgba(255,0,0,0.1), rgba(255,240,240,.9))`
           }}>
-          <div className='hide'>
-            <a
-              className='proj-title'
-              href={proj.deployed}
-              target="_blank"
-              rel="noreferrer">
-                {proj.title}
-            </a>
             <a
               className='repo-link'
               href={proj.repo}
               target="_blank"
               rel="noreferrer"
-            >
+            > Repo
               <img
                 src={gitLogo}
                 alt="git logo"
+                width="50"
+                heigth="50">
+              </img>
+            </a>
+            <a
+              className='deployed-link'
+              href={proj.deployed}
+              target="_blank"
+              rel="noreferrer"
+            > App
+              <img
+                src={liveLogo}
+                alt="live app logo"
                 width="50"
                 heigth="50">
               </img>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ResumePDF from '../../assets/Campbell_Resume2022_WebDev.pdf';
+import downloadLogo from '../../assets/download-logo.png';
 
 export default function Resume() {
   return (
     <div>
       <h1>→ Resume</h1>
       <p>
-        <a href={ResumePDF} download>Download resume as PDF file</a> 
+        <a className='download-link' href={ResumePDF} download>
+          Download resume as PDF file
+            <img
+                src={downloadLogo}
+                alt="download logo"
+                width="40"
+                heigth="40">
+            </img>
+        </a> 
       </p>
       <p>Education</p>
       <ul>
