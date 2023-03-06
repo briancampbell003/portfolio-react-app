87a200f18d67943e61355e41d7d6ca220f3eea59
Brian Campbell <brian.campbell003@gmail.com>
2023-03-05 21:48:17 -0600

    added mhl project

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -4,17 +4,27 @@ import DreamJotBG from './assets/dreamJot.png';
 import TeamGenBG from './assets/teamGen.png';
 import TechBuzzBG from './assets/techBuzz.png';
 import WeatherDashBG from './assets/weatherDash.png';
+import MhlReloBG from './assets/mhlRelo.png';
 import VaristasDigitalCafeBG from './assets/varistasDigitalCafe.png';
 import NoteTakerBG from './assets/noteTaker.png';
 
 export default function Projects() {
   const projData = [
     {
+      title: 'MHL Relocation Guide',
+      deployed: 'https://briancampbell003.github.io/mhl-rs-relocation/',
+      repo: 'https://github.com/briancampbell003/mhl-rs-relocation',
+      imgPath: MhlReloBG,
+      key: '01',
+      description: 'Informative React app built for Resident Services Staff at Mercy Housing Lakefront',
+      tech: 'JavaScript, ReactJS, HMTL, CSS'
+    },
+    {
       title: 'Dream Jot',
       deployed: 'https://dream-jot.herokuapp.com/',
       repo: 'https://github.com/briancampbell003/dream-jot',
       imgPath: DreamJotBG,
-      key: '01',
+      key: '02',
       description: 'Digital dream journal with community features',
       tech: 'Sequelize, ExpressJS, Handlebars'
     },
@@ -23,19 +33,19 @@ export default function Projects() {
       deployed: 'https://briancampbell003.github.io/module-10-team-profile-demo/',
       repo: 'https://github.com/briancampbell003/module-10-team-profile-generator',
       imgPath: TeamGenBG,
-      key: '02',
-      description: 'Commeand-line application to create stylized page provided team member info',
+      key: '03',
+      description: 'Command-line application to create stylized page once provided with team member info',
       tech: 'Jest, NodeJS, Inquirer, CSS'
     },
-    {
-      title: 'Weather Dash',
-      deployed: 'https://briancampbell003.github.io/weather-dash/',
-      repo: 'https://github.com/briancampbell003/weather-dash',
-      imgPath: WeatherDashBG,
-      key: '03',
-      description: 'Forecast provider using API call to weather app based on user input',
-      tech: 'JavaScript, HMTL, CSS'
-    },
+    // {
+    //   title: 'Weather Dash',
+    //   deployed: 'https://briancampbell003.github.io/weather-dash/',
+    //   repo: 'https://github.com/briancampbell003/weather-dash',
+    //   imgPath: WeatherDashBG,
+    //   key: '03',
+    //   description: 'Forecast provider using API call to weather app based on user input',
+    //   tech: 'JavaScript, HMTL, CSS'
+    // },
     {
       title: 'Varistas Digital Cafe',
       deployed: 'https://briancampbell003.github.io/varistas-digital-cafe/',
